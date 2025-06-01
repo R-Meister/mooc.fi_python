0e89d99cf017872eddaaf8c75e8fa4601f08ea76
R-Meister <raman.66852@gmail.com>
2025-06-01 11:00:32 +0530

    Part 1 done, week == part, now onto week 2/part 2

--- part01-30_quadratic_formula/src/quadratic_formula.py
+++ part01-30_quadratic_formula/src/quadratic_formula.py
@@ -4,3 +4,11 @@ from math import sqrt
 
 # Note that the square root can also be calculated using power.
 # sqrt(9) is equivalent to 9 ** 0.5
+a = int(input("Value of a: "))
+b = int(input("Value of b: "))
+c = int(input("Value of c: "))
+print("")
+x = ((-1*b) + sqrt((b*b)-4*a*c))/(2*a)
+y = ((-1*b) - sqrt((b*b)-4*a*c))/(2*a)
+
+print(f"The roots are {x} and {y}")
