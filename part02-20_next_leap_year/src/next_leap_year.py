91971d5d9e2fe5cb1ec16e2b11f3ee74e8726326
R-Meister <raman.66852@gmail.com>
2025-06-09 12:50:57 +0530

    Some more excercises

--- part02-20_next_leap_year/src/next_leap_year.py
+++ part02-20_next_leap_year/src/next_leap_year.py
@@ -1,1 +1,14 @@
 # Write your solution here
+year = int(input("Year: "))
+while True:
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year %400 == 0:
+                isLeapYear = True
+            elif year %400 != 0:
+                isLeapYear = False
+        else isLeapYear = True
+    
+    else:
+        isLeapYear = False
+
