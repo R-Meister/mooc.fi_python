d0f538b92a5b3d6a7da532fd9aa868944d2b1d21
R-Meister <raman.66852@gmail.com>
2025-06-10 08:36:48 +0530

    While loop leap year

--- part02-20_next_leap_year/src/next_leap_year.py
+++ part02-20_next_leap_year/src/next_leap_year.py
@@ -1,14 +1,28 @@
 # Write your solution here
 year = int(input("Year: "))
+currentyear1 = year
+
 while True:
+    year += 1  
     if year % 4 == 0:
         if year % 100 == 0:
-            if year %400 == 0:
+            if year % 400 == 0:
                 isLeapYear = True
-            elif year %400 != 0:
+                ishundred = True
+            else:
                 isLeapYear = False
-        else isLeapYear = True
-    
+                ishundred = True
+        else:
+            isLeapYear = True
+            ishundred = False
     else:
         isLeapYear = False
+        ishundred = False
 
+    if isLeapYear:
+        break
+
+if isLeapYear == True and ishundred == True:
+    print(f"The next leap year after {currentyear1} is {year}")
+elif isLeapYear == True and ishundred == False:
+    print(f"The next leap year after {currentyear1} is {year}")
