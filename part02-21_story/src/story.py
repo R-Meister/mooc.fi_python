457015ec8d40df626fc2137b5303dfcf8b5ca121
R-Meister <raman.66852@gmail.com>
2025-06-15 22:10:04 +0530

    done with part 2

--- part02-21_story/src/story.py
+++ part02-21_story/src/story.py
@@ -1,1 +1,14 @@
 # Write your solution here
+story = ""
+word = ''
+lastword = ''
+while True:
+    story += (f"{word} ")
+    word = str(input("Please type in a word: "))
+    if word == 'end':
+        print(story)
+        break
+    if word == lastword:
+        print(story)
+        break
+    lastword = word
--- part02-22_working_with_numbers/src/working_with_numbers.py
+++ part02-22_working_with_numbers/src/working_with_numbers.py
@@ -1,1 +1,24 @@
 # Write your solution here
+print("Please type in integer numbers. Type in 0 to finish.")
+count = 0
+sum = 0
+pcount = 0
+ncount = 0
+mean = 0
+while True:
+    num = int(input("Number: "))
+    if num == 0:
+        break
+    sum += num
+    count +=1
+    if num < 0:
+        ncount += 1
+    elif num > 0:
+        pcount += 1
+    mean = float(sum/count)
+print(f"Numbers typed in {count}")
+print(f"The sum of the numbers is {sum}")
+print(f"The mean of the numbers is {mean}")
+print(f"Positive numbers {pcount}")
+print(f"Negative numbers {ncount}")
+
